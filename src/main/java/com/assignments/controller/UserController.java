5a476ca402ecb584e607127778f41b11247cfcd2
이의정 <stxtory@gmail.com>
2024-11-09 11:20:35 +0900

    리팩토링: 비즈니스 로직 userService로 빼기

--- src/main/java/com/assignments/controller/UserController.java
+++ src/main/java/com/assignments/controller/UserController.java
@@ -2,12 +2,10 @@ package com.assignments.controller;
 
 import com.assignments.domain.entity.User;
 import com.assignments.domain.vo.response.UserResponse;
-import com.assignments.repository.UserRepository;
+import com.assignments.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,69 +15,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 public class UserController extends BaseController {
 
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getUsers() {
-        return ResponseEntity.ok(
-                userRepository.findAllByDeletedAtIsNull().stream()
-                        .map(UserResponse::of)
-                        .collect(Collectors.toList()));
+        return ResponseEntity.ok(userService.getAll());
     }
 
     @PostMapping
     public ResponseEntity<UserResponse> addUser(@RequestBody User user) {
-        Optional<User> optionalUser = userRepository.findByUsernameAndDeletedAtIsNull(user.getUsername());
-        if (optionalUser.isPresent()) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return ResponseEntity.status(HttpStatus.CREATED).body(UserResponse.of(userRepository.save(user)));
+        if (userService.isDuplicateByUsername(user.getUsername())) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
     @GetMapping("/{id}")
-    @Transactional(readOnly = true)
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
-        return userRepository.findByIdAndDeletedAtIsNull(id)
+        return userService.get(id)
                 .filter(user -> getAuthenticationName().filter(name -> name.equals(user.getUsername())).isPresent())
-                .map(UserResponse::ofWithPost)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @PutMapping("/{id}")
-    @Transactional
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
-        return userRepository.findByIdAndDeletedAtIsNull(id)
-                .filter(user -> getAuthenticationName().filter(name -> name.equals(user.getUsername())).isPresent())
-                .map(user -> {
-                    user.setName(updatedUser.getName());
-                    user.setGender(updatedUser.getGender());
-                    user.setAge(updatedUser.getAge());
-                    user.setPhone(updatedUser.getPhone());
-                    user.setUpdatedAt(LocalDateTime.now());
-                    return ResponseEntity.ok(UserResponse.of(user));
-                })
+        if (userService.get(id).filter(user -> getAuthenticationName().filter(name -> name.equals(user.getUsername())).isPresent()).isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return userService.update(id, updatedUser)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @DeleteMapping("/{id}")
-    @Transactional
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        userRepository.findByIdAndDeletedAtIsNull(id)
-                .filter(user -> getAuthenticationName().filter(name -> name.equals(user.getUsername())).isPresent())
-                .ifPresent(user -> user.setDeletedAt(LocalDateTime.now()));
-//        userRepository.deleteById(id);
+        if (userService.get(id).filter(user -> getAuthenticationName().filter(name -> name.equals(user.getUsername())).isPresent()).isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        userService.delete(id);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/assignments/service/UserService.java
+++ src/main/java/com/assignments/service/UserService.java
@@ -1,19 +1,30 @@
 package com.assignments.service;
 
 import com.assignments.domain.entity.User;
+import com.assignments.domain.vo.response.UserResponse;
 import com.assignments.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-
+@Transactional(readOnly = true)
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(@Autowired UserRepository userRepository, @Autowired PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -25,5 +36,43 @@ public class UserService implements UserDetailsService {
                 .password(user.getPassword())
                 .authorities("USER") // 권한 설정
                 .build();
+    }
+
+    public List<UserResponse> getAll() {
+        return userRepository.findAllByDeletedAtIsNull().stream()
+                .map(UserResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isDuplicateByUsername(String username) {
+        return userRepository.findByUsernameAndDeletedAtIsNull(username).isPresent();
+    }
+
+    public UserResponse save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return UserResponse.of(userRepository.save(user));
+    }
+
+    public Optional<UserResponse> get(Long id) {
+        return userRepository.findByIdAndDeletedAtIsNull(id).map(UserResponse::ofWithPost);
+    }
+
+    @Transactional
+    public Optional<UserResponse> update(Long id, User updatedUser) {
+        return userRepository.findByIdAndDeletedAtIsNull(id)
+                .map(user -> {
+                    user.setName(updatedUser.getName());
+                    user.setGender(updatedUser.getGender());
+                    user.setAge(updatedUser.getAge());
+                    user.setPhone(updatedUser.getPhone());
+                    user.setUpdatedAt(LocalDateTime.now());
+                    return UserResponse.of(user);
+                });
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        userRepository.findByIdAndDeletedAtIsNull(id)
+                .ifPresent(user -> user.setDeletedAt(LocalDateTime.now()));
     }
 }
